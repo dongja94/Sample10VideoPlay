d30ffc3a34d8450d2b4c228d66f318bdaf7e285b
dongja94 <dongja94@gmail.com>
2016-03-11 10:15:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplevideoplay/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dongja94.samplevideoplay;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = (VideoView)findViewById(R.id.videoView);
+        MediaController controller = new MediaController(this);
+        videoView.setMediaController(controller);
+        videoView.setVideoURI(Uri.parse("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"));
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+    }
+}
